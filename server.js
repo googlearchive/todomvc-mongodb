62f5cc82d03ddac3f34498e8f40b57ef75e6d40e
Mirna Bouchra Megally <mirnabouchra@google.com>
2014-10-17 20:01:16 +0200

    Add license comments to source files and fix LICENSE file

--- server.js
+++ server.js
@@ -1,4 +1,26 @@
-// server.js
+/**
+ * @license
+ * Everything in this repo is MIT License unless otherwise specified.
+ *
+ * Copyright (c) Addy Osmani, Sindre Sorhus, Pascal Hartig, Stephen  Sawchuk, "Google, inc.".
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 	// set up ========================
 	var express  = require('express');
@@ -13,8 +35,8 @@
 
 	mongoose.connect('mongodb://' + argv.be_ip + ':80/my_database');
 
-    app.use('/js', express.static(__dirname + '/js'));
-    app.use('/bower_components', express.static(__dirname + '/bower_components'));
+    	app.use('/js', express.static(__dirname + '/js'));
+   	 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 	app.use(morgan('dev')); 										// log every request to the console
 	app.use(bodyParser.urlencoded({'extended':'true'})); 			// parse application/x-www-form-urlencoded
 	app.use(bodyParser.json()); 									// parse application/json
